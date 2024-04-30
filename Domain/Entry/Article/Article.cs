6803623e95f7e53cfdeed5a143a0b1e04b46b0d2
Vinicius <viniciusgabriel.z.lucas@gmail.com>
2024-04-29 22:21:11 -0300

    Att

--- Domain/Entry/Article/Article.cs
+++ Domain/Entry/Article/Article.cs
@@ -24,6 +24,7 @@
         public long AuthorId { get; set; }
         public long AdvisorId { get; set; }
         public long CoAdvisorId { get; set; }
+        public DateTime DevolutionDate { get; set; }
 
         #region VirtualPropeties
         #region Internal
--- Domain/Entry/User/User.cs
+++ Domain/Entry/User/User.cs
@@ -13,7 +13,9 @@
 
         public string Name { get; set; }
         public string Email { get; set; }
+        public string PrivateEmail { get; set; }
         public string Password { get; set; }
+        public DateTime BindingDate { get; set; }
 
         #region VirtualProperties
         #region External
