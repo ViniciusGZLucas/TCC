3e95fafd212ed683f3719261363895596dc9963e
Molina <biel_molina9@hotmail.com>
2024-06-17 19:45:01 -0300

    [Molina] - Adicionando co orientador no get article

--- Domain/ViewModel/Article/ArticleDeliveryDateViewModel.cs
+++ Domain/ViewModel/Article/ArticleDeliveryDateViewModel.cs
@@ -6,6 +6,7 @@
         public string? Title { get; set; }
         public string? Author { get; set; }
         public string? Advisor { get; set; }
+        public string? CoAdvisor { get; set; }
         public string? Description { get; set; }
         public string? AdvisorCurriculumLink { get; set; }
         public string? CoAdvisorCurriculumLink { get; set; }
